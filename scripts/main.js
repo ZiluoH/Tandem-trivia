3c7f51c6890e63f545ad1da462123025552a0a8a
Richard Huang <ziluoh@gmail.com>
2020-11-01 16:33:30 -0800

    Finish JS

--- scripts/main.js
+++ scripts/main.js
@@ -46,30 +46,24 @@ class Quiz{
     //updata content of answer
     initialAnswers(){
         let answersDiv = document.getElementById('answers');   //get answers div
+        answersDiv.innerHTML = "";
         //get all choice base on current question idx
         let allChoice = this.data[this.questionList[this.questionCounter]].incorrect.concat(this.data[this.questionList[this.questionCounter]].correct);
-        if (answersDiv.childElementCount == 0) {
-            for (let i = 0; i < allChoice.length; i++) {
-                const choiceInput = document.createElement('input');
-                const choiceLabel = document.createElement('label');
-                const choiceDiv = document.createElement('div');
-                choiceInput.setAttribute('type','radio');
-                choiceInput.setAttribute('name','answer');
-                choiceInput.setAttribute('value', allChoice[i]);
-                choiceInput.setAttribute('id', `choice${i}`);
-                choiceLabel.setAttribute('for', `choice${i}`);
-                choiceLabel.textContent = allChoice[i];
-                choiceDiv.appendChild(choiceInput);
-                choiceDiv.appendChild(choiceLabel);
-                answersDiv.appendChild(choiceDiv);
-            }
-        } else {
-            for (let i = 0; i < allChoice.length; i++) {
-                const choiceInput = document.querySelector(`[id = choice${i}]`);
-                const choiceLabel = document.querySelector(`[for = choice${i}]`);
-                choiceInput.value = allChoice[i];
-                choiceLabel.textContent = allChoice[i];
-            }
+        // if (answersDiv.childElementCount == 0) {
+        for (let i = 0; i < allChoice.length; i++) {
+            const choiceInput = document.createElement('input');
+            const choiceLabel = document.createElement('label');
+            const choiceDiv = document.createElement('div');
+            choiceInput.setAttribute('type','radio');
+            choiceInput.setAttribute('name','answer');
+            choiceInput.setAttribute('value', allChoice[i]);
+            choiceInput.setAttribute('id', `choice${i}`);
+            choiceLabel.setAttribute('for', `choice${i}`);
+            choiceLabel.textContent = allChoice[i];
+            choiceDiv.appendChild(choiceInput);
+            choiceDiv.appendChild(choiceLabel);
+            answersDiv.appendChild(choiceDiv);
+
             let answers = document.getElementsByName('answer');
             for(let answer of answers){
                 if(this.chosenAnswers[this.questionCounter] === null){
@@ -101,6 +95,13 @@ class Quiz{
             this.initialQuestion();
             this.initialAnswers();
         }
+        if(this.questionCounter>0){
+            document.getElementById('prev').style.display = 'inline-block';
+        }
+        if(this.questionCounter === 9){
+            document.getElementById('next').style.display = 'none';
+            document.getElementById('submit').style.display = 'inline-block';
+        }
     }
 
     //get the previous question and answer update chosen answer
@@ -111,24 +112,39 @@ class Quiz{
             this.initialQuestion();
             this.initialAnswers();
         }
+        if(this.questionCounter === 0){
+            document.getElementById('prev').style.display = 'none';
+        }
+        if(this.questionCounter < 9){
+            document.getElementById('submit').style.display = 'none';
+            document.getElementById('next').style.display = 'inline-block';
+        }
     }
 
     //submit quiz and get the total score
     submitQuiz(){
+        this.getCheckedAnswer();
         //generate the correct answer
         let correctAnswer = [];
         for(let i = 0; i< this.questionList.length;i++){
-            correctAnswer.push(this.data[i].correct);
+            correctAnswer.push(this.data[this.questionList[i]].correct);
         }
 
         //check if all question is answered
-        for(let i = 0; i< this.chosenAnswers.length;i++){
-            if(this.chosenAnswers[i] === null){
-                alert('You need to answer all question before submitting quiz');
-                return;
-            }else if(this.chosenAnswers[i] === correctAnswer[i]){
-                this.correctCounter += 1;
+        if(this.chosenAnswers.every((answer) => answer !== null)){
+            for(let i = 0; i< this.chosenAnswers.length;i++){
+                if(this.chosenAnswers[i] === correctAnswer[i]){
+                    this.correctCounter += 1;
+                }
             }
+            document.getElementById('answers').innerHTML = "";
+            document.getElementById('questionContent').textContent = `You score ${this.correctCounter} out of 10 questions.`
+            document.getElementById('start').style.display = 'inline-block';
+            document.getElementById('prev').style.display = 'none';
+            document.getElementById('submit').style.display = 'none';
+        }else{
+            alert('You need to answer all question before submitting quiz');
+            return;
         }
     }
 }
@@ -151,5 +167,22 @@ let quiz = new Quiz();
 window.addEventListener('DOMContentLoaded', () => {
     quiz.fetchData();
 })
+
+function start(){
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('next').style.display = 'inline-block';
+    quiz = new Quiz();
+    fetch('data.json').then(function(response){
+        return response.json()
+    }).then(function(obj){
+        quiz.fetchData(obj);
+        quiz.generateQuestionIdx();
+        quiz.initialQuestion();
+        quiz.initialAnswers();
+    }).catch(function(error){
+        console.error('Something went wrong with retrieving the data!')
+        console.error(error)
+    })
+}
 
 window.quiz = quiz
